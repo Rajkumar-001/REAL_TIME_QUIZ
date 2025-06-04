7940f82980f1c41ad6459a948f393f1adcadde94
Raj Kumar Pandey <raj.pandey@gocomet.com>
2025-06-04 14:57:41 +0530

    WIP: working changes before rebase

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,16 @@
-import { server as WebSocketServer } from 'websocket';
-import * as http from 'http';
+import { IoManager } from "./managers/IoManager";
 
+const io = IoManager.getIo();
 
-var server = http.createServer(function (request: any, response: any) {
-    console.log((new Date()) + ' received request for the url ' + request.url)
-    response.writeHead(404);
-    response.end();
-})
+io.listen(3000)
 
-server.listen(8080, function () {
-    console.log((new Date()) + ' server is listening on the port');
-})
+io.on('connection', (client) => {
+    client.on('event', data => {
+        const _ = data.type;
 
-const wsServer = new WebSocketServer({
-    httpServer: server,
-    autoAcceptConnections: false,
+    })
+    client.on('disconnect', () => {
+
+    });
+
 });
-
-function originIsAllowed(origin: string) {
-    return true;
-}
-
-wsServer.on('request', function (request: any) {
-    if (!originIsAllowed(request.origin)) {
-        request.reject();
-        console.log((new Date()) + ' request rejection ');
-        return;
-    }
-
-    var connection = request.accept('echo-protocol', request.origin);
-    console.log((new Date()) + " Connection Accepted ");
-    connection.on('message', function (message: any) {
-        if (message.type == 'utf8') {
-            console.log("Received Message " + message.utf8Data)
-            connection.sendUtf8(message.utf8Data);
-        } else if (message.type == 'binary') {
-            console.log('Received Message ' + message.binaryData)
-            connection.sendBinaryData('Recieved Message ' + message.binaryData)
-        }
-
-    });
-
-    connection.on('close', function (reasonCode: any, description: any) {
-        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-    });
-})
-
-
-
-
-
