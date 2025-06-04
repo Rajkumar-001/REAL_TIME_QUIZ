d0d36602deec742e53a7652fae1f058374851ab2
Raj Kumar Pandey <raj.pandey@gocomet.com>
2025-06-04 15:01:07 +0530

    added the README File

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { IoManager } from "./managers/IoManager";
 
+
 const io = IoManager.getIo();
 
 io.listen(3000)
@@ -7,10 +8,8 @@ io.listen(3000)
 io.on('connection', (client) => {
     client.on('event', data => {
         const _ = data.type;
-
     })
     client.on('disconnect', () => {
 
     });
-
 });
